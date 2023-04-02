6250875e0ece009f359eb6f33e36e65f59e9b8cd
Dantesuito <deadblood404@hotmail.com>
2023-04-02 19:37:04 +0200

    Nombres de métodos refactorizados para incluir nuestro nombre en ellos.

--- src/Coche.java
+++ src/Coche.java
@@ -1,11 +1,11 @@
 public class Coche {
     public int velocidad;
 
-    public void acelerar(int aceleracion) {
+    public void acelerar_Daniel_Vital_Torres(int aceleracion) {
         velocidad += aceleracion;
     }
 
-    public void decelerar(int deceleracion) {
+    public void decelerar_Daniel_Vital_Torres(int deceleracion) {
         velocidad -= deceleracion;
         if (velocidad < 0){
             velocidad = 0;
